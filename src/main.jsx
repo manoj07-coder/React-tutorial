f361717aa4cf61c7a5b7ba36aa025c268e076a07
Manoj <manojgowdacs35@gmail.com>
2023-12-03 22:36:45 +0530

    bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,37 +3,21 @@ import {createRoot} from 'react-dom/client';
 
 import './main.css'
 
-const firstBook = {
-  img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203._SY75_.jpg',
-  title: 'The Kite Runner',
-  author: 'Khaled Hosseini'
-}
-
-const secondBook = {
-  img : 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590282886i/9777._SY75_.jpg',
-  title: 'The God of Small Things',
-  author: 'Arundhati Roy'
-}
-
+import {books} from './books'
+import Book from './Book'
+  
 
 function BookList(){
   return(
     <section className="bookList">
-    <Book img= {firstBook.img} title= {firstBook.title} author= {firstBook.author} />
-    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+    {books.map((book) => {
+      return <Book key={book.id} {...book} />
+    })}
     </section>
   )
-}
+}      
 
-const Book = (props)=> {
-  return (
-    <article className="book">
-      <img src={props.img} />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
-    </article>
-  )
-}
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(<BookList />)
