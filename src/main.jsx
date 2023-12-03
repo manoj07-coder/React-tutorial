e9ecca8c55d0008b3a81514c9d52c7c4445f6076
Manoj <manojgowdacs35@gmail.com>
2023-12-03 14:22:08 +0530

    component rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
 
-function Book(){
-  return (
-    <section>
-      <Image />
-      <Title />
-      <Author />
+import './main.css' 
+
+const title = 'The Kite Runner';
+const author = 'Khaled Hosseini';
+const img = 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203._SY75_.jpg'
+
+function BookList(){
+  return(
+    <section className="bookList">
+    <Book job='developer'/>
+    <Book title='random title' number={7}/>
     </section>
   )
 }
 
-const Image = () => {
-  return <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203._SY75_.jpg" alt="BookImage" />
-}
-
-const Title = () => {
-  return <h4>The Kite Runner</h4>
-}
-
-const Author = () => {
-  return <h4>Khaled Hosseini</h4>
+const Book = (props)=> {
+  return (
+    <article className="book">
+      <img src={img} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
+    </article>
+  )
 }
 
 const root = createRoot(document.getElementById('root'));
-root.render(<Book />)
+root.render(<BookList />)
