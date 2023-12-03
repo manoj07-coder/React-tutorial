9f63f0c2f925a0efa884c7633557c149daf2bc29
Manoj <manojgowdacs35@gmail.com>
2023-12-03 14:28:50 +0530

    rendering using props

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
 
-import './main.css' 
+import './main.css'
 
-const title = 'The Kite Runner';
-const author = 'Khaled Hosseini';
-const img = 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203._SY75_.jpg'
+const firstBook = {
+  img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203._SY75_.jpg',
+  title: 'The Kite Runner',
+  author: 'Khaled Hosseini'
+}
+
+const secondBook = {
+  img : 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590282886i/9777._SY75_.jpg',
+  title: 'The God of Small Things',
+  author: 'Arundhati Roy'
+}
+
 
 function BookList(){
   return(
     <section className="bookList">
-    <Book job='developer'/>
-    <Book title='random title' number={7}/>
+    <Book img= {firstBook.img} title= {firstBook.title} author= {firstBook.author} />
+    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
     </section>
   )
 }
@@ -19,12 +28,9 @@ function BookList(){
 const Book = (props)=> {
   return (
     <article className="book">
-      <img src={img} />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
-      <p>{props.number}</p>
+      <img src={props.img} />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
