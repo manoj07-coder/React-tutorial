a956d330d3a0bdf0f2f8059aad3b60a9f36145c4
Manoj <manojgowdacs35@gmail.com>
2023-12-03 22:36:17 +0530

    books list

---
+++ src/books.jsx
@@ -0,0 +1,20 @@
+export const books = [
+    {
+      id: 1,
+      img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203._SY75_.jpg',
+      title: 'The Kite Runner',
+      author: 'Khaled Hosseini'
+    },
+    {
+      id:2,
+      img : 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590282886i/9777._SY75_.jpg',
+      title: 'The God of Small Things',
+      author: 'Arundhati Roy'
+    },
+    {
+      id:3,
+      img : 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388176242i/24._SY75_.jpg',
+      title: 'In a Sunburned Country',
+      author: 'Bill Bryson'
+    }
+  ]
